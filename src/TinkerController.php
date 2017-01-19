d701dc6c16514510565b45e597a19008b976e337
罗晓俊 <luoxiaojun@luoxiaojundeMac-mini.local>
2017-01-19 11:19:46 +0800

    init commit

---
+++ src/TinkerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yii2Tinker;
+
+use \yii\console\Controller;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class TinkerController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     */
+    public function actionIndex()
+    {
+        $_SERVER['argv'] = [];
+
+        // And go!
+        call_user_func(\Psy\bin());
+    }
+}
